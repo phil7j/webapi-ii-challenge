ba53c1c27876c01ecdccd10a993f6e2f6fe3939e
Philip Johnson <phil7j@gmail.com>
2019-08-06 15:36:40 -0700

    All Functionality Complete, only testing remaining

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -31,11 +31,53 @@ router.get('/:id', (req,res)=>{
 
     data.findById(id)
         .then(response => {
-            !response ? res.status(404).json({message: "The post with the specified ID does not exist."}) :
-            res.status(200).json(response)
+          !response ? res.status(404).json({message: "The post with the specified ID does not exist."}) :
+          res.status(200).json(response)
         })
-        .catch( err = {
+        .catch( err => {
             res.status(500).json({error: "The post information could not be retrieved."})
+        })
+})
+
+router.delete('/:id', (req,res)=> {
+    const id = req.params.id;
+    // first find by ID
+   let post = data.findById(id)
+        .then( response => {
+            !response ? res.status(404).json({message: "The post with the specified ID does not exist."}):
+            res.status(200);
+            return response;
+        }).catch( err => {
+            res.status(500).json({error: "The post could not be removed"})
+        })
+    //delete post and return it
+      data.remove(id)
+        .then( response => {
+            res.status(200).json(post);
+        })
+        .catch( err => {
+            res.status(500).json({error: "The post could not be removed" })
+        })
+})
+
+router.put('/id', (req,res) => {
+    const id = req.params.id;
+    const {title, contents} = req.body;
+
+    !title || !contents ? res.status(400).json({ errorMessage: "Please provide title and contents for the post."}) :
+    data.findById(id)
+        .then( post => {
+            data.update(id,req.body)
+                .then( response => {
+                    res.status(200).json(post)
+                })
+                .catch( err => {
+                    res.status(500).json({error: "The post information could not be modified." })
+                })
+        }
+        )
+        .catch( err => {
+            res.status(404).json({message: "The post with the specified ID does not exist."})
         })
 })
 
